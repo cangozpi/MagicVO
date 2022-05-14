13338c2f6189790fb32d50221691a71448025639
cangozpi <cangozpinar@gmail.com>
2022-05-14 11:28:24 +0300

    fix cnn_backbone_model padding values

--- models/CNN_Backbone_src/CNN_Backbone_model.py
+++ models/CNN_Backbone_src/CNN_Backbone_model.py
@@ -6,23 +6,23 @@ class CNN_backbone_model(nn.Module):
         super().__init__()
         self.device = "gpu" if torch.cuda.is_available() else "cpu"
         self.model = torch.nn.Sequential(
-            torch.nn.Conv2d(6, 64, 7, 2),
+            torch.nn.Conv2d(6, 64, 7, 2, padding=3),
             torch.nn.LeakyReLU(0.1),
-            torch.nn.Conv2d(64, 128, 5, 2),
+            torch.nn.Conv2d(64, 128, 5, 2, padding=2),
             torch.nn.LeakyReLU(0.1),
-            torch.nn.Conv2d(128, 256, 5, 2),
+            torch.nn.Conv2d(128, 256, 5, 2, padding=2),
             torch.nn.LeakyReLU(0.1),
-            torch.nn.Conv2d(256, 256, 3, 1),
+            torch.nn.Conv2d(256, 256, 3, 1, padding=1),
             torch.nn.LeakyReLU(0.1),
-            torch.nn.Conv2d(256, 512, 3, 2),
+            torch.nn.Conv2d(256, 512, 3, 2, padding=1),
             torch.nn.LeakyReLU(0.1),
-            torch.nn.Conv2d(512, 512, 3, 1),
+            torch.nn.Conv2d(512, 512, 3, 1, padding=1),
             torch.nn.LeakyReLU(0.1),
-            torch.nn.Conv2d(512, 512, 3, 2),
+            torch.nn.Conv2d(512, 512, 3, 2, padding=1),
             torch.nn.LeakyReLU(0.1),
-            torch.nn.Conv2d(512, 512, 3, 1),
+            torch.nn.Conv2d(512, 512, 3, 1, padding=1),
             torch.nn.LeakyReLU(0.1),
-            torch.nn.Conv2d(512, 1024, 3, 2),
+            torch.nn.Conv2d(512, 1024, 3, 2, padding=1),
             torch.nn.LeakyReLU(0.1)
         ).to(self.device)
 
