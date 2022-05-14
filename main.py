b0f9e92c5c173022a1d2dcaa082f882837566915
cangozpi <cangozpinar@gmail.com>
2022-05-14 05:52:28 +0300

    training is adapted to handle using cnn_backbone_model. There is problem with the kernel_size,stride,padding values in the cnn_back_bone class

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from data import VisualOdometryDataset
 import torch
-from models.FlowNet2_src import FlowNet2S
-from models.MagicVO_src import MagicVO_model
-from models.CNN_Backbone_src import CNN_Backbone_model
-from .utils.train import train
+# from models.FlowNet2_src import FlowNet2S
+from models.MagicVO_src.MagicVO_model import MagicVO_Model
+from models.CNN_Backbone_src.CNN_Backbone_model import CNN_backbone_model
+from utils.train import train_with_flownet, train_with_cnn_backbone
 
 
 def main():
@@ -16,7 +16,7 @@ def main():
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=2, shuffle=False)
     val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=2)
     
-    flownet_or_CNN_backbone = True # if True use flownet, if not use CNN_backbone
+    flownet_or_CNN_backbone = False # if True use flownet, if not use CNN_backbone
 
     if flownet_or_CNN_backbone: # Use FlowNet as the feature extractor
         # Load pre-trained FlowNet model
@@ -29,24 +29,37 @@ def main():
             pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
             model_dict.update(pretrained_dict)
             flownet2_model.load_state_dict(model_dict)
+    
+        # Initialize MagicVO model
+        magicVO_model = MagicVO_Model()
+
+        # Train the model
+        epochs = 5
+        lr = 1e-3
+        k = 1 #TODO check its value from the paper
+        train_flownet = False # Whether to freeze the layers of flownet during training
+        magicVO_ckpt_path = "./models/checkpoints/magicVO_best_val_ckpt.pth.tar" # Path to save the magicVO model checkpoints
+        flownet_ckpt_path = "./models/checkpoints/flownet_best_val_ckpt.pth.tar" # Path to save the flownet model checkpoints
+        load_flownet_ckpt = False # Load flownet from a ckpt saved during training
+        load_magicVO_ckpt = False # Load magicvo from a ckpt saved during training
+        train_with_flownet(flownet2_model, magicVO_model, train_dataloader, val_dataloader, epochs, lr, k, flownet_ckpt_path, magicVO_ckpt_path,\
+            train_flownet=train_flownet, load_flownet_ckpt=load_flownet_ckpt, load_magicVO_ckpt=load_magicVO_ckpt)
     else: # Use CNN_Backbone as the feature extractor
-        cnn_backbone_model = CNN_Backbone_model()
+        # Initialize CNN_Backbone_model 
+        cnn_backbone_model = CNN_backbone_model()
+        # Initialize MagicVO model
+        magicVO_model = MagicVO_Model()
 
-    # Initialize MagicVO model
-    magicVO_model = MagicVO_model()
-
-    # Train the model
-    epochs = 5
-    lr = 1e-3
-    k = 1 #TODO check its value from the paper
-    train_flownet = False # Whether to freeze the layers of flownet during training
-    magicVO_ckpt_path = "./models/checkpoints/magicVO_best_val_ckpt.pth.tar" # Path to save the magicVO model checkpoints
-    flownet_ckpt_path = "./models/checkpoints/flownet_best_val_ckpt.pth.tar" # Path to save the flownet model checkpoints
-    load_flownet_ckpt = False # Load flownet from a ckpt saved during training
-    load_magicVO_ckpt = False # Load magicvo from a ckpt saved during training
-    train(flownet2_model, magicVO_model, train_dataloader, val_dataloader, epochs, lr, k, flownet_ckpt_path, magicVO_ckpt_path,\
-         train_flownet=train_flownet, load_flownet_ckpt=load_flownet_ckpt, load_magicVO_ckpt=load_magicVO_ckpt)
-
+        # Train the model
+        epochs = 5
+        lr = 1e-3
+        k = 1 #TODO check its value from the paper
+        magicVO_ckpt_path = "./models/checkpoints/magicVO_best_val_ckpt.pth.tar" # Path to save the magicVO model checkpoints
+        cnn_backbone_ckpt_path = "./models/checkpoints/flownet_best_val_ckpt.pth.tar" # Path to save the cnn_backbone model checkpoints
+        load_cnn_backone_ckpt = False # Load cnn_backbone from a ckpt saved during training
+        load_magicVO_ckpt = False # Load magicvo from a ckpt saved during training
+        train_with_cnn_backbone(cnn_backbone_model, magicVO_model, train_dataloader, val_dataloader, epochs, lr, k, cnn_backbone_ckpt_path, magicVO_ckpt_path,\
+            load_cnn_backone_ckpt=load_cnn_backone_ckpt, load_magicVO_ckpt=load_magicVO_ckpt)
 
 
 if __name__ == "__main__":
--- utils/train.py
+++ utils/train.py
@@ -18,7 +18,7 @@ def plot_results(train_losses, val_losses):
 
     plt.show()
 
-def train(flownet_model, magicVO_model, train_dataloader, val_dataloader, epochs, lr, k, flownet_ckpt_path, magicVO_ckpt_path,\
+def train_with_flownet(flownet_model, magicVO_model, train_dataloader, val_dataloader, epochs, lr, k, flownet_ckpt_path, magicVO_ckpt_path,\
      train_flownet=False, load_flownet_ckpt=False, load_magicVO_ckpt=False):
     optim_flownet = torch.optim.Adagrad(flownet_model.parameters(), lr)
     optim_magicVO = torch.optim.Adagrad(magicVO_model.parameters(), lr)
@@ -28,7 +28,7 @@ def train(flownet_model, magicVO_model, train_dataloader, val_dataloader, epochs
     loaded_loss = None
     # Load magicVO_model & optimizer from ckpt
     if load_flownet_ckpt:
-        checkpoint = torch.load(magicVO_ckpt_path)
+        checkpoint = torch.load(flownet_ckpt_path)
         flownet_ckpt_path.load_state_dict(checkpoint['flownet_model_state_dict'])
         optim_flownet.load_state_dict(checkpoint['flownet_optimizer_state_dict'])
         loaded_epoch = checkpoint['epoch']
@@ -136,6 +136,129 @@ def train(flownet_model, magicVO_model, train_dataloader, val_dataloader, epochs
                 'flownet_optimizer_state_dict': optim_flownet.state_dict(),
                 'loss': avg_val_loss,
             }, flownet_ckpt_path)
+            # Save MagicVO
+            torch.save({
+                'epoch': i,
+                'magicVO_model_state_dict': magicVO_model.state_dict(),
+                'magicVO_optimizer_state_dict': optim_magicVO.state_dict(),
+                'loss': avg_val_loss,
+            }, magicVO_ckpt_path)
+            print("*" * 20)
+            print(f"Saved model checkpoint at epoch: {i+loaded_epoch} with Validation Loss: {avg_val_loss}")
+        else:
+             print(f'] Epoch: {i+loaded_epoch}/{epochs}, \tTraining Loss: {avg_train_loss}, \tValidation Loss: {avg_val_loss}')
+
+    # ======== Plot Training Results
+    plot_results(train_losses, val_losses)
+
+
+
+def train_with_cnn_backbone(cnn_backbone_model, magicVO_model, train_dataloader, val_dataloader, epochs, lr, k, cnn_backbone_ckpt_path, magicVO_ckpt_path,\
+            load_cnn_backone_ckpt=False, load_magicVO_ckpt=False):
+    optim_cnn_backbone = torch.optim.Adagrad(cnn_backbone_model.parameters(), lr)
+    optim_magicVO = torch.optim.Adagrad(magicVO_model.parameters(), lr)
+
+    # ======== Load models from ckpts
+    loaded_epoch = 0
+    loaded_loss = None
+    # Load magicVO_model & optimizer from ckpt
+    if load_cnn_backone_ckpt:
+        checkpoint = torch.load(cnn_backbone_ckpt_path)
+        cnn_backbone_ckpt_path.load_state_dict(checkpoint['cnn_backbone_model_state_dict'])
+        optim_cnn_backbone.load_state_dict(checkpoint['cnn_backbone_optimizer_state_dict'])
+        loaded_epoch = checkpoint['epoch']
+        loaded_loss = checkpoint['loss']
+        print(f"Loaded cnn_backbone_model from saved ckpt. \tloaded epoch:{loaded_epoch}, \t loaded best validation loss: {loaded_loss}")
+    
+    # Load cnn_backbone_model & optimizer from ckpt
+    if load_magicVO_ckpt:
+        checkpoint = torch.load(magicVO_ckpt_path)
+        magicVO_model.load_state_dict(checkpoint['magicVO_model_state_dict'])
+        optim_magicVO.load_state_dict(checkpoint['magicVO_optimizer_state_dict'])
+        loaded_epoch = checkpoint['epoch']
+        loaded_loss = checkpoint['loss']
+        print(f"Loaded magicVO_model from saved ckpt. \tloaded epoch:{loaded_epoch}, \t loaded best validation loss: {loaded_loss}")
+    
+
+
+    # ======== Use GPU if available
+    device = "gpu" if torch.cuda.is_available() else "cpu"
+    cnn_backbone_model = cnn_backbone_model.to(device)
+    magicVO_model = magicVO_model.to(device)
+
+
+
+    print('=' * 30)
+    print('Training MagicVO model')
+    print('=' * 30)
+
+    train_losses = [] # i^th index holds the avg training loss for the i^th epoch
+    val_losses = [] # i^th index holds the avg validation loss for the i^th epoch
+    best_val_loss = None if loaded_loss == None else loaded_loss # best validation loss (used during saving checkpoints)
+    # Train/Validate the model
+    for i in range(epochs- loaded_epoch): # train for #epochs
+        # ======== Train
+        cnn_backbone_model.train()
+        magicVO_model.train()
+        cum_train_loss = 0
+        for step, (img_cat, odometry) in enumerate(train_dataloader): # per batch training
+            img_cat = img_cat.to(device)
+            odometry = odometry.to(device)
+            
+            optim_cnn_backbone.zero_grad()
+            optim_magicVO.zero_grad()
+
+            # Extract image features using FlowNet/CNNs
+            print(img_cat.shape, "img_cat.shape =========== DEBUG")
+            out = cnn_backbone_model(img_cat)
+            # make 6 DoF predictions using MagicVO_model
+            train_loss = magicVO_model.loss(out, odometry, k)
+
+            # Backpropagate the gradients
+            train_loss.backward()
+            optim_cnn_backbone.step()
+            optim_magicVO.step()
+
+
+            
+            cum_train_loss += train_loss.item() * len(img_cat) # convert average loss back to total loss
+        
+        # Record the training loss
+        avg_train_loss = cum_train_loss / len(train_dataloader.dataset)
+        train_losses.append(avg_train_loss)
+
+        
+        # ======== Validate
+        cnn_backbone_model.eval()
+        magicVO_model.eval()
+        cum_val_loss = 0
+        for img_cat, odometry in val_dataloader: # per batch evaluation
+            img_cat = img_cat.to(device)
+            odometry = odometry.to(device)
+            
+            with torch.no_grad():
+                # Extract image features using FlowNet/CNNs
+                out = cnn_backbone_model(img_cat)
+                # make 6 DoF predictions using MagicVO_model
+                val_loss = magicVO_model.loss(out, odometry, k)
+
+                
+                cum_val_loss += val_loss.item() * len(img_cat) # convert average loss back to total loss
+        
+        # Record the validation loss
+        avg_val_loss = cum_val_loss / len(val_dataloader.dataset)
+        val_losses.append(avg_val_loss)
+
+
+        # ======== Save Checkpoints
+        if (best_val_loss == None) or (best_val_loss >= avg_val_loss):
+            # Save cnn_backbone
+            torch.save({
+                'epoch': i,
+                'cnn_backbone_model_state_dict': cnn_backbone_model.state_dict(),
+                'cnn_backbone_optimizer_state_dict': optim_cnn_backbone.state_dict(),
+                'loss': avg_val_loss,
+            }, cnn_backbone_ckpt_path)
             # Save MagicVO
             torch.save({
                 'epoch': i,
